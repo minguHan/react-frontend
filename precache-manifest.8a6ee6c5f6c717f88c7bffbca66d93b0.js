0908326ae3906a9eab6c65216eadcb06c40972b6
hanminqu <hanminqu@gmail.com>
2022-04-27 17:11:16 +0900

    Updates

---
+++ precache-manifest.8a6ee6c5f6c717f88c7bffbca66d93b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696d4340a09b0ca47cfe997ac6d23645",
+    "url": "/minguHan/react-frontend/index.html"
+  },
+  {
+    "revision": "55eab7a1abaad709f149",
+    "url": "/minguHan/react-frontend/static/css/2.a1f65902.chunk.css"
+  },
+  {
+    "revision": "95e3c9cd3cff6b0844e6",
+    "url": "/minguHan/react-frontend/static/css/main.4d1ea2ce.chunk.css"
+  },
+  {
+    "revision": "55eab7a1abaad709f149",
+    "url": "/minguHan/react-frontend/static/js/2.b8455aa6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/minguHan/react-frontend/static/js/2.b8455aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e3c9cd3cff6b0844e6",
+    "url": "/minguHan/react-frontend/static/js/main.10085cf1.chunk.js"
+  },
+  {
+    "revision": "6a4da1cd2fc81dc470e4",
+    "url": "/minguHan/react-frontend/static/js/runtime-main.9866be83.js"
+  }
+]);
